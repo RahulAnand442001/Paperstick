fbcd88d0abc87029fbb2bd5afae01ea8cd51750e
RahulAnand442001 <sahurahul442001@gmail.com>
2021-04-03 23:11:23 +0530

    local storage added

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const colors = ["#0ef074", "#adabdf", "#d1df0bbb", "#f12234", "#3cdf0bbb"];
 
 const body = document.getElementById("body");
 const editor = document.getElementById("editor");
-let htmlContainer = document.getElementById("html_container");
 
 // change background of page randomly
 let randomColor = colors[Math.floor(Math.random() * colors.length)];
 body.style.background = randomColor;
+
+// set to local storage
+document.addEventListener("click", (e) => {
+  localStorage.setItem("text-in-editor", editor.innerText);
+});
+
+// remove from local storage on exit
+document.onclose(() => localStorage.removeItem("text-in-editor"));
+
 
 // download the editor text;
 document.getElementById("download").onclick = () => {
